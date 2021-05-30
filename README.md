3475110ad1834ea6698426e9200ee4f701d49884
linckn <lincknick@gmail.com>
2021-05-30 15:47:11 -0400

    adding the TwitterAPI class and updating the README

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # TwitterAPIExamples
 A demonstration of how to use Python to access the Twitter API
+This is meant to be starter code for anyone who wants to build an application using the Twitter API and python.
+
+TwitterAPI.py contains all of the code required to pull tweets from a specific user and then pull all of the replies to that given tweet. 
+
+## Getting Started
+Go to https://developer.twitter.com/ and create a developer account in order to get a bearer token to access the API.
+
+Once you have a bearer token, 'git clone' this repository.
+
+## Quick Setup
+If you want to get up an running without setting up your own python environment. Go to https://jupyter.org/try and click 'try classic notebook'. 
+
+Create a new cell with the '+' icon and copy the contents of TwitterAPI.py from the cloned repository into the newly created cell.
+
+In the function 'create_headers' follow the instructions in the code in order to add your bearer token to the code. Alternatively you can set an enviroment variable so that you do not need to include your bearer token in the code. NEVER post your bearer token publicly! 
+
+Now press the play button at the top of the notebook while you have the cell selected that contains the TwitterAPI.py code. 
+
+You should see output from your favorite tweeter! (Control output in '__main__' by setting variables 'DEBUG' and 'INFO' to 'True' or 'False')
+
+## Details
+'process_tweets()' is the main function that will call other helper functions from the Twitter API class. Modify this if you would like to do something new with the code. 'process_tweets()' is meant to be an example for your to build your own application. The helpwer functions 'get_tweets(user)' and 'get_replies(id)' are where the main functionality of the Twitter API is stored.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[Apache License](http://www.apache.org/licenses/)
---
+++ TwitterAPI.py
@@ -0,0 +1,122 @@
+import requests
+import os
+import json
+
+# To set your environment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+
+
+# The TwitterAPI Class allows you to pull tweets from a specific user and
+# to see the replies to the tweets from that user
+class TwitterAPI():
+    
+    def __init__(self):
+        self.bearer_token = self.auth()
+        
+        
+    ############################################
+    ### Twitter API Request Helper Functions ###
+    ############################################
+    def auth(self):
+        return os.environ.get("BEARER_TOKEN")
+    
+    def create_headers(self):
+        headers = {"Authorization": "Bearer {}".format(self.bearer_token)}
+        logging_debug(headers)
+        # if you did not set "BEARER_TOKEN" as environment variable, 
+        # type in you bearer token manually here in place of '{}'
+        #headers = {"Authorization": "{}"}
+        #logging_debug(headers)
+        return headers
+
+
+    def create_url(self, query):    
+        # Tweet fields are adjustable.
+        # Options include:
+        # attachments, author_id, context_annotations,
+        # conversation_id, created_at, entities, geo, id,
+        # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+        # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+        # source, text, and withheld
+        tweet_fields = "tweet.fields=author_id"
+        url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
+            query, tweet_fields
+        )
+        return url
+
+
+    def connect_to_endpoint(self, url, headers):
+        response = requests.request("GET", url, headers=headers)
+        logging(response.status_code)
+        if response.status_code != 200:
+            raise Exception(response.status_code, response.text)
+        return response.json()
+
+
+
+    ##########################################
+    ### Functions to Get Data from Twitter ###
+    ##########################################
+    def get_tweets(self, user):
+        # '-is:retweet' means that retweets from this conversation will not be included
+        # change 'lexfridman' to any use you would like to get tweets from
+        url = self.create_url(query="from:" + user + " -is:retweet")
+        headers = self.create_headers()
+        json_response = self.connect_to_endpoint(url, headers)
+        logging_info("get_tweets returned: " + print_json(json_response))
+        return json_response
+
+
+    # TODO: if the original tweet is a reply to someone else. this does not work.  
+    def get_replies(self, id):
+        url = self.create_url(query="conversation_id:" + id + " -is:retweet")
+        headers = self.create_headers()
+        json_response = self.connect_to_endpoint(url, headers)
+        return json_response
+
+
+    ##########################################
+    ### Functions to Process Returned Data ###
+    ##########################################
+    def process_tweets(self):
+        # get all of the tweets from a specific user
+        json_response = self.get_tweets("lexfridman")
+
+        count = 0
+        for tweet in json_response["data"]:            
+            logging_info(("tweet ", count, ": ", tweet["text"]))
+
+            # get all of the replies from the initial tweet (using conversation id)
+            replies = self.get_replies(tweet["id"])
+            logging_info("replies: " + print_json(replies))
+            count += 1
+
+
+########################
+### Helper Functions ###
+########################
+def print_json(json_response):
+    return json.dumps(json_response, indent=4, sort_keys=True)
+
+def logging_debug(message):
+    if DEBUG == True:
+        print(message)
+        
+def logging_info(message):
+    if INFO == True:
+        print(message)
+
+        
+#####################
+### Main Function ###
+#####################
+if __name__ == "__main__":
+    DEBUG = False # set to True in order to contain more information
+    INFO = True
+
+    twitter_api = TwitterAPI()
+    twitter_api.process_tweets()
+
+
+
+
